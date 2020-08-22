5a31fe7d5815e7448f983b1754674523fd3f7b8c
sepehr abdoli <sepehrskater@yahoo.com>
2020-08-22 15:02:28 +0200

    deleted home controller

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -17,6 +17,13 @@ class TweetsController extends Controller
             'body' => $attributes['body']
         ]);
 
+        public function index()
+    {
+        return view('home', [
+            'tweets' => auth()->user()->timeline()
+        ]);
+    }
+
         return redirect('/home');
     }
 }
