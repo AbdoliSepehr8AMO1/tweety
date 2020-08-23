cf5c892d257a69389e3aff010b826a3426a83168
sepehr abdoli <sepehrskater@yahoo.com>
2020-08-23 14:12:48 +0200

    users paginate to the set of 50 / you will see max of 50 users on the explore page

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -6,6 +6,8 @@ use App\User;
 
 class ExploreController extends Controller
 {
+
+    // users paginate to the set of 50 / you will see max of 50 users on the explore page
     public function __invoke()
     {
         return view('explore', [
